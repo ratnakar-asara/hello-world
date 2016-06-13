8845271874dd33dbddeba1d47ec63d4ded3ef3c5
Ratnakar <asara_ratnakar@yahoo.co.in>
2016-06-13 13:22:50 -0400

    Update hello-world.go

--- hello-world.go
+++ hello-world.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("############# Hello World ###############")
+	args := os.Args
+	if (len(args) < 3) {
+		fmt.Println("########### Expecting two Argumemnts ... Exiting")
+		return
+	}
+	fmt.Println("############### Arguments supplied are :", args[1],args[2])	
 }
